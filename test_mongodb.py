a90fa7a107d687c6c3d9dcfbd750f78ddb0fcb2c
Arvind <aravindavp79@gmail.com>
2026-03-17 16:40:13 +0530

    Fixed personal security issue

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,10 +1,10 @@
 
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-uri = "mongodb+srv://db_name:<db_password>@cluster0.urgx8sw.mongodb.net/?appName=Cluster0"
+
+uri = "mongodb+srv://<username>:<password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
 try:
